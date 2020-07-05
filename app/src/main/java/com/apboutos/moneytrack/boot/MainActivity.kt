5f73d98e6bfda0b9ec156aa4c0165030c4e67807
exophrenik <exophrenik@gmail.com>
2020-07-05 23:48:52 +0300

    Main testing
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/boot/MainActivity.kt
+++ app/src/main/java/com/apboutos/moneytrack/boot/MainActivity.kt
@@ -2,18 +2,23 @@ package com.apboutos.moneytrack.boot
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import com.apboutos.moneytrack.R
 import com.apboutos.moneytrack.model.database.database.MoneytrackDatabase
 import com.apboutos.moneytrack.model.entity.Entry
 
 class MainActivity : Activity() {
+
+    private val text by lazy {findViewById<TextView>(R.id.helloTextBox)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val e = Entry.createEmptyEntry()
 
-        val ee = MoneytrackDatabase.invoke(this)
+        //val ee = MoneytrackDatabase.invoke()
 
+        text.text = "Yolo"
     }
 }
