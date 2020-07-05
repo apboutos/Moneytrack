9ef36d39215c720470ea2004a2f6c07ec341d6ff
exophrenik <exophrenik@gmail.com>
2020-07-05 18:24:23 +0300

    MoneytrackDatabase.kt implementation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/boot/MainActivity.kt
+++ app/src/main/java/com/apboutos/moneytrack/boot/MainActivity.kt
@@ -3,6 +3,7 @@ package com.apboutos.moneytrack.boot
 import android.app.Activity
 import android.os.Bundle
 import com.apboutos.moneytrack.R
+import com.apboutos.moneytrack.model.database.database.MoneytrackDatabase
 import com.apboutos.moneytrack.model.entity.Entry
 
 class MainActivity : Activity() {
@@ -11,5 +12,7 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         val e = Entry.createEmptyEntry()
+
+        val ee = MoneytrackDatabase.invoke(this)
     }
 }
