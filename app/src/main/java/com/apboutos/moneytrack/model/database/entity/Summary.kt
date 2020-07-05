59484a75a57b1535f3670a70093d62896d0ee5d7
exophrenik <exophrenik@gmail.com>
2020-07-05 23:17:22 +0300

    Summary.kt implementation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

---
+++ app/src/main/java/com/apboutos/moneytrack/model/database/entity/Summary.kt
@@ -0,0 +1,16 @@
+package com.apboutos.moneytrack.model.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Summary(
+        val username : String,
+        val category: String,
+        val type : String,
+        val fromDate : String,
+        val untilDate : String)
+{
+    @PrimaryKey(autoGenerate = true)
+    var id : Int = 0
+}
