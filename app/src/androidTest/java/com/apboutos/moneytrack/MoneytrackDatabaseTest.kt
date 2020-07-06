9ed3ed39607fc51032b14e07f5cde743e573e247
exophrenik <exophrenik@gmail.com>
2020-07-06 23:19:52 +0300

    Database Testing.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

---
+++ app/src/androidTest/java/com/apboutos/moneytrack/MoneytrackDatabaseTest.kt
@@ -0,0 +1,115 @@
+package com.apboutos.moneytrack
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.core.app.ApplicationProvider
+import android.content.Context
+import android.database.sqlite.SQLiteConstraintException
+import androidx.room.Room
+import com.apboutos.moneytrack.model.database.dao.*
+import com.apboutos.moneytrack.model.database.database.MoneytrackDatabase
+import com.apboutos.moneytrack.model.database.entity.Entry
+import com.apboutos.moneytrack.model.database.entity.User
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class MoneytrackDatabaseTest{
+
+    private lateinit var db: MoneytrackDatabase
+    private lateinit var entryDao : EntryDAO
+    private lateinit var userDao  : UserDAO
+    private lateinit var categoryDAO: CategoryDAO
+    private lateinit var summaryDAO: SummaryDAO
+    private lateinit var credentialDAO: CredentialDAO
+
+    @Before
+    fun createDb() {
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, MoneytrackDatabase::class.java).build()
+        userDao = db.UserDAO()
+        entryDao = db.EntryDAO()
+        categoryDAO = db.CategoryDAO()
+        summaryDAO = db.SummaryDAO()
+        credentialDAO = db.CredentialDAO()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertUser() {
+        val user = User("exophrenik","ma582468","exophrenik@gmail.com","2020-07-06","2020-07-6 23:43:23")
+            userDao.insert(user)
+        val byName : User = userDao.selectUserBy("exopdhrenik")
+        Assert.assertEquals(null,byName)
+    }
+
+    @Test(expected = SQLiteConstraintException::class)
+    @Throws(Exception::class)
+    fun insertDuplicateUserAndRead() {
+        val user = User("exophrenik","ma582468","exophrenik@gmail.com","2020-07-06","2020-07-6 23:43:23")
+        userDao.insert(user)
+        userDao.insert(user)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun updateUser() {
+        val user = User("exophrenik","ma582468","exophrenik@gmail.com","2020-07-06","2020-07-6 23:43:23")
+        userDao.insert(user)
+        val byName : User = userDao.selectUserBy("exophrenik")
+        Assert.assertEquals(user,byName)
+        val user2 = User("exophrenik","ma582470","exophrenik@gmail.com","2020-07-06","2020-07-6 23:43:23")
+        userDao.update(user2)
+        val byName2 = userDao.selectUserBy("exophrenik")
+        Assert.assertNotEquals(byName,byName2)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun deleteUser() {
+        val user = User("exophrenik","ma582470","exophrenik@gmail.com","2020-07-06","2020-07-6 23:43:23")
+        userDao.insert(user)
+        val byName : User = userDao.selectUserBy("exophrenik")
+        Assert.assertEquals(user,byName)
+        userDao.delete(user)
+        Assert.assertEquals(null,userDao.selectUserBy("exophrenik"))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun selectAllEntriesOfDate(){
+
+        val list1 : List<Entry> = listOf(
+            Entry("ex2213","exophrenik","Expense","toEat","junk food",92.21,"2020-06-01","2020-06-01",false),
+            Entry("ex2214","exophrenik","Income","paycheck","paycheck",92.21,"2020-06-01","2020-06-01",false),
+            Entry("ex2215","exophrenik","Expense","toEat","junk food",92.21,"2020-06-02","2020-06-02",false),
+            Entry("ex2216","exophrenik","Expense","toEat","junk food",92.21,"2020-06-02","2020-06-02",false),
+            Entry("ex2217","exophrenik","Expense","toEat","junk food",92.21,"2020-06-02","2020-06-02",false))
+            for(i in list1){
+                entryDao.insert(i)
+            }
+        val list2 = entryDao.selectAllEntriesOfDate("2020-06-01","exophrenik")
+        Assert.assertNotEquals(list1,list2)
+        val list3 = mutableListOf<Entry>()
+        list3.addAll(list2)
+        list3.addAll(entryDao.selectAllEntriesOfDate("2020-06-02","exophrenik"))
+        Assert.assertEquals(list1,list3)
+
+        val date = Date()
+
+    }
+
+
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+}
