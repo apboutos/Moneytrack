06e9d666bd6a29a8a4c6419fd40e12ff2d9131de
exophrenik <exophrenik@gmail.com>
2020-07-10 17:27:39 +0300

    NetworkTester.kt implementation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

---
+++ app/src/main/java/com/apboutos/moneytrack/model/repository/remote/NetworkTester.kt
@@ -0,0 +1,25 @@
+@file:Suppress("unused")
+
+package com.apboutos.moneytrack.model.repository.remote
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
+//TODO Find a non deprecated way
+object NetworkTester {
+    fun internetConnectionIsAvailable(context: Context): Boolean {
+        var haveConnectedWifi = false
+        var haveConnectedMobile = false
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo: Array<NetworkInfo>
+        if (cm != null) {
+            netInfo = cm.allNetworkInfo
+            for (ni in netInfo) {
+                if (ni.typeName.equals("WIFI", ignoreCase = true)) if (ni.isConnected) haveConnectedWifi = true
+                if (ni.typeName.equals("MOBILE", ignoreCase = true)) if (ni.isConnected) haveConnectedMobile = true
+            }
+        }
+        return haveConnectedWifi || haveConnectedMobile
+    }
+}
