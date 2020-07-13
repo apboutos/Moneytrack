d1e9595276b92c00e1804c45ee47e3eae1ac39b8
exophrenik <exophrenik@gmail.com>
2020-07-13 21:28:15 +0300

    Time.kt implementation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

---
+++ app/src/main/java/com/apboutos/moneytrack/utilities/Time.kt
@@ -0,0 +1,18 @@
+package com.apboutos.moneytrack.utilities
+
+import android.icu.text.SimpleDateFormat
+import com.apboutos.moneytrack.model.database.converter.Date
+import com.apboutos.moneytrack.model.database.converter.Datetime
+import java.util.*
+
+object Time {
+
+    fun getTimestamp() : Datetime{
+        return Datetime(SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(java.util.Date()))
+    }
+
+    fun getDate() : Date{
+        return Date(SimpleDateFormat("yyyy-MM-dd", Locale.US).format(java.util.Date()))
+    }
+
+}
