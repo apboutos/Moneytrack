5fb0e7a999d61d08c0c1df002ea04dbc58674f81
exophrenik <exophrenik@gmail.com>
2020-09-12 18:25:07 +0300

    LedgerReceiver.kt implementation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/viewmodel/receiver/LedgerReceiver.kt
+++ app/src/main/java/com/apboutos/moneytrack/viewmodel/receiver/LedgerReceiver.kt
@@ -3,9 +3,22 @@ package com.apboutos.moneytrack.viewmodel.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.apboutos.moneytrack.model.database.entity.Entry
+import com.apboutos.moneytrack.view.LedgerActivity
 
-class LedgerReceiver : BroadcastReceiver() {
+
+class LedgerReceiver(private val parentActivity: LedgerActivity) : BroadcastReceiver() {
+
+    private val tag = "LedgerReceiver"
     override fun onReceive(context: Context?, intent: Intent?) {
 
+        val list = intent?.getParcelableArrayListExtra<Entry>("entryList")
+        parentActivity.viewModel.updateDatabaseWithReceivedRemoteEntries(list)
+
+    }
+
+    companion object {
+        const val SERVER_PULL_DATA_RESPONSE = "server"
+        const val SERVER_PUSH_DATA_RESPONSE = "server"
     }
 }
