448288ac243c9f95db267555f2fa181b0a850009
exophrenik <exophrenik@gmail.com>
2020-12-03 13:47:01 +0200

    Added @expose annotations.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/model/database/entity/User.kt
+++ app/src/main/java/com/apboutos/moneytrack/model/database/entity/User.kt
@@ -2,14 +2,15 @@ package com.apboutos.moneytrack.model.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
 
 @Entity(tableName = "user")
 data class User(
 
     @PrimaryKey(autoGenerate = false)
-    var username         : String,
-    var password         : String,
-    var email            : String,
+    @Expose var username         : String,
+    @Expose var password         : String,
+    @Expose var email            : String,
     var registrationDate : String,  // format: yyyy-MM-dd
     var lastLogin        : String)  // format: yyyy-MM-dd HH-mm-ss
 {
