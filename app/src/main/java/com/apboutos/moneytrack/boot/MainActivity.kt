40086ef060b0a04ebf28f1400269633410b1282a
exophrenik <exophrenik@gmail.com>
2020-12-05 16:50:21 +0200

    TODO management.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/boot/MainActivity.kt
+++ app/src/main/java/com/apboutos/moneytrack/boot/MainActivity.kt
@@ -31,4 +31,20 @@ class MainActivity : Activity() {
         }
         finish()
     }
+
+
+    //TODO Feature  : Implement the push data mechanism so the entries deleted by the user are also marked as deleted in the server.
+    //TODO Feature  : The settings dialog must be implemented. Example settings 1. Confirm before swipe delete. 2. Optional ledger legend. 3. Theme change
+    //TODO Feature  : Implement a progress bar dialog that appears while the user is waiting for login authentication.
+    //TODO Feature  : Server domain must move to https
+    //TODO Bug      : When the user does a search and the description box is left empty no entry is returned instead of returning all entries.
+    //                To fix this when the description box is empty it must be passed as null instead of an empty string ""
+    //TODO Bug      : Login is taking too long. Must look into it.
+    //TODO Bug      : In the search dialog the user has no way to return to the current day. Arrow buttons must be implemented.
+    //TODO Bug      : After using search the arrow buttons that change date disappear. They also disappear for certain long dates.
+    //TODO Bug      : In the ReportDialog there is a small bug that must be investigated.
+    //                When the date is 29 of February and the year changes to a non leap year the day defaults to 1 instead of 28.
+    //TODO Refactor : Find a non deprecated way to implement the NetworkTester Class
+
+
 }
