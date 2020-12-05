70198920fbf4bd4765eed417e9983989428baa73
exophrenik <exophrenik@gmail.com>
2020-12-05 14:13:42 +0200

    DatetimeDeserializer.kt implementation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

---
+++ app/src/main/java/com/apboutos/moneytrack/model/database/converter/DatetimeDeserializer.kt
@@ -0,0 +1,17 @@
+package com.apboutos.moneytrack.model.database.converter
+
+import android.util.Log
+import com.google.gson.*
+import java.lang.reflect.Type
+
+class DatetimeDeserializer() : JsonDeserializer<Datetime> {
+
+    val tag = "DatetimeDeserializer"
+
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Datetime {
+        if(json == null) throw JsonParseException("Datetime in json is null")
+        Log.d(tag,"json was deserialized as ${json.asJsonObject.get("datetime")}")
+
+        return Datetime(json.asJsonObject.get("datetime").toString())
+    }
+}
