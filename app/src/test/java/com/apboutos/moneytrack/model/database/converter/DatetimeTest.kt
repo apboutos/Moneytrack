070d1c0faf6dbc783eaa67be9ead752e4d1c0838
exophrenik <exophrenik@gmail.com>
2021-04-01 08:28:05 +0300

    currentDatetime UnitTest.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/test/java/com/apboutos/moneytrack/model/database/converter/DatetimeTest.kt
+++ app/src/test/java/com/apboutos/moneytrack/model/database/converter/DatetimeTest.kt
@@ -2,11 +2,25 @@ package com.apboutos.moneytrack.model.database.converter
 
 import org.junit.Assert.*
 import org.junit.Test
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class DatetimeTest{
 
     @Test
     fun testIsBefore(){
         assertTrue(Datetime("2020-09-04 21:00:00").isBefore(Datetime("2020-09-05 21:00:00")))
+    }
+
+    @Test
+    fun currentDatetime(){
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+
+        assertEquals(Datetime.currentDatetime(),current.format(formatter))
+        println(current.format(formatter))
+        println(Datetime.currentDatetime())
     }
 }
