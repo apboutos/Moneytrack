43d326efbf884d4ab15bfdc2b55b57abe6822809
exophrenik <exophrenik@gmail.com>
2021-04-01 15:56:00 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/utilities/converter/CurrencyConverter.kt
+++ app/src/main/java/com/apboutos/moneytrack/utilities/converter/CurrencyConverter.kt
@@ -7,13 +7,23 @@ object CurrencyConverter {
 
     const val tag = "CurrencyConverter"
 
+    /**
+     * Returns a String in the containing the specified amount formatted
+     * in the standard US currency format and applies the specified currency symbol
+     * at the end.
+     */
     fun toPresentableAmount(amount: Double, currency: String) : String{
         if(amount == 0.0) return "0.00 $"
-        val formatter = DecimalFormat("#,###.00");
+        val formatter = DecimalFormat("##,###.00");
 
         return "${formatter.format(amount)} $currency"
     }
 
+    /**
+     * Returns a double containing a normalized value of the specified amount.
+     * All the decimal points over two decimal digits are dropped and a sign
+     * is applied depending on the specified type.
+     */
     fun normalizeAmount(amount : Double , type : String) : Double{
         val tmp = if(amount < 0 && type == "Income") amount*(-1)
         else if(amount >= 0 && type == "Income") amount
