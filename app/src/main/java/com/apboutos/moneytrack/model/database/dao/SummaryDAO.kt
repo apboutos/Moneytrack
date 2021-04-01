a51642e6f4e39104d96e1adbffbee0df87eb89fb
exophrenik <exophrenik@gmail.com>
2021-04-01 09:27:20 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/model/database/dao/SummaryDAO.kt
+++ app/src/main/java/com/apboutos/moneytrack/model/database/dao/SummaryDAO.kt
@@ -8,12 +8,21 @@ import com.apboutos.moneytrack.model.database.entity.Summary
 @Dao
 interface SummaryDAO {
 
+    /**
+     * Inserts a new Summary.
+     */
     @Insert
     fun insert(summary : Summary)
 
+    /**
+     * Deletes all summaries.
+     */
     @Query("DELETE FROM summary")
     fun deleteAllSummaries()
 
+    /**
+     * Returns a list containing all existing summaries.
+     */
     @Query("SELECT * FROM summary")
     fun selectAllSummaries() : List<Summary>
 }
