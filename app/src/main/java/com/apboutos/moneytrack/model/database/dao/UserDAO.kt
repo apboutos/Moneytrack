ec9ed8bd58c2f4476c8177c3cebbd9e7297f3dbf
exophrenik <exophrenik@gmail.com>
2021-04-01 09:30:34 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/model/database/dao/UserDAO.kt
+++ app/src/main/java/com/apboutos/moneytrack/model/database/dao/UserDAO.kt
@@ -7,15 +7,27 @@ import com.apboutos.moneytrack.model.database.entity.User
 @Dao
 interface UserDAO {
 
+    /**
+     * Inserts a new User.
+     */
     @Insert
     fun insert(user : User)
 
+    /**
+     * Updates an existing User.
+     */
     @Update
     fun update(user : User)
 
+    /**
+     * Deletes an existing User.
+     */
     @Delete
     fun delete(user : User)
 
+    /**
+     * Returns a User with the matching username otherwise returns null.
+     */
     @Query("SELECT * FROM user WHERE username = :username")
-    fun selectUserBy(username : String) : User
+    fun selectUserBy(username : String) : User?
 }
