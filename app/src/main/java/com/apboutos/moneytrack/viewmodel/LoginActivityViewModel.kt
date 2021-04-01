9ce77421e3aa013603187a6ed17e1534fe5fa810
exophrenik <exophrenik@gmail.com>
2021-04-01 13:52:14 +0300

    Code refactoring and documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/viewmodel/LoginActivityViewModel.kt
+++ app/src/main/java/com/apboutos/moneytrack/viewmodel/LoginActivityViewModel.kt
@@ -4,43 +4,80 @@ package com.apboutos.moneytrack.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.apboutos.moneytrack.model.database.entity.Credential
+import com.apboutos.moneytrack.model.database.entity.User
 import com.apboutos.moneytrack.model.repository.local.DatabaseRepository
 import com.apboutos.moneytrack.model.repository.remote.NetworkTester
 import com.apboutos.moneytrack.model.repository.remote.OnlineRepository
 import com.apboutos.moneytrack.utilities.error.LoginError
+import kotlinx.coroutines.launch
 
 class LoginActivityViewModel(application: Application) : AndroidViewModel(application) {
 
-        private val databaseRepository = DatabaseRepository(application)
-        private val onlineRepository = OnlineRepository(application)
+    private val databaseRepository = DatabaseRepository(application)
+    private val onlineRepository = OnlineRepository(application)
 
-        fun retrieveStoredCredential() : Credential? = databaseRepository.selectCredential()
+    /**
+     * Retrieves the stored Credential from the local database.
+     * Returns null if no Credential is found.
+     */
+    fun retrieveStoredCredential(): Credential? {
+        var credential: Credential? = null
+        viewModelScope.launch {
+            credential = databaseRepository.selectCredential()
+        }
+        return credential
+    }
 
-        fun requestAuthentication( username : String, password : String) : LoginError {
+    /**
+     *  Authenticates the specified username and password against the local repository.
+     *  If the authentication fails the remote repository is used. If that fails as well
+     *  and authentication error is produced.
+     */
+    fun requestAuthentication(username: String, password: String): LoginError {
 
-            val user = databaseRepository.selectUserBy(username)
-            // user == null means that the user was not found in the local database
-            // and so an online login attempt must be made.
-            if(user == null){
-                if(!NetworkTester.internetConnectionIsAvailable(getApplication())) return LoginError.NO_INTERNET
-                onlineRepository.authenticateUser(username,password)
-                return LoginError.ATTEMPTING_ONLINE_LOGIN
-            }else{
-                return when {
-                    user.username != username -> {LoginError.WRONG_USERNAME}
-                    user.password != password -> {LoginError.WRONG_PASSWORD}
-                    else -> {LoginError.NO_ERROR}
+        var user : User? = null
+        viewModelScope.launch { user = databaseRepository.selectUserBy(username) }
+        // user == null means that the user was not found in the local database
+        // and so an online login attempt must be made.
+        if (user == null) {
+            if (!NetworkTester.internetConnectionIsAvailable(getApplication())) return LoginError.NO_INTERNET
+            onlineRepository.authenticateUser(username, password)
+            return LoginError.ATTEMPTING_ONLINE_LOGIN
+        }
+        else {
+            return when {
+                user!!.username != username -> {
+                    LoginError.WRONG_USERNAME
+                }
+                user!!.password != password -> {
+                    LoginError.WRONG_PASSWORD
+                }
+                else -> {
+                    LoginError.NO_ERROR
                 }
             }
         }
+    }
 
-        fun saveUserCredential( username: String, password: String){
-                databaseRepository.deleteCredential()
-                databaseRepository.insertCredential(Credential(username,password))
+    /**
+     * Saves the user's Credential to the local repository.
+     */
+    fun saveUserCredential( username: String, password: String){
+        viewModelScope.launch {
+            databaseRepository.deleteCredential()
+            databaseRepository.insertCredential(Credential(username,password))
         }
+    }
 
-        fun deleteStoredCredential(){
-                databaseRepository.deleteCredential()
+    /**
+     * Deletes the stored Credential from the local repository.
+     */
+    fun deleteStoredCredential(){
+        viewModelScope.launch {
+            databaseRepository.deleteCredential()
         }
+    }
+
 }
