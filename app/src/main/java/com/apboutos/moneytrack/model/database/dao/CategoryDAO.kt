3b244f48621d0a5b7b1654f278ca0bbfa34e84f9
exophrenik <exophrenik@gmail.com>
2021-04-01 08:55:19 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/model/database/dao/CategoryDAO.kt
+++ app/src/main/java/com/apboutos/moneytrack/model/database/dao/CategoryDAO.kt
@@ -6,15 +6,27 @@ import com.apboutos.moneytrack.model.database.entity.Category
 @Dao
 interface CategoryDAO {
 
+    /**
+     * Inserts a new Category.
+     */
     @Insert
     fun insert(category: Category)
 
+    /**
+     * Updates an existing Category.
+     */
     @Update
     fun update(category: Category)
 
+    /**
+     *Deletes an existing Category.
+     */
     @Delete
     fun delete(category: Category)
 
+    /**
+     * Returns a list containing all the existing categories.
+     */
     @Query("SELECT * FROM category")
     fun selectAllCategories() : List<Category>
 
