552e9ff6654b04952509242857a47ec1d1c12ee0
exophrenik <exophrenik@gmail.com>
2021-04-02 08:41:34 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/view/LedgerActivity.kt
+++ app/src/main/java/com/apboutos/moneytrack/view/LedgerActivity.kt
@@ -79,6 +79,9 @@ class LedgerActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Provides the recycler view with on item touch detection functionality.
+     */
     private fun registerRecyclerViewListener(){
         adapter.listener = object : LedgerRecyclerAdapter.OnItemClickListener{
             override fun onItemClick(position: Int) {
@@ -90,6 +93,9 @@ class LedgerActivity : AppCompatActivity() {
     }
 
 
+    /**
+     * Opens a dialog to create a new entry.
+     */
     private fun openNewDialog(){
         val dialog = NewEntryDialog(this)
         dialog.setCanceledOnTouchOutside(false)
@@ -97,12 +103,19 @@ class LedgerActivity : AppCompatActivity() {
         dialog.show()
     }
 
+    /**
+     * Opens a dialog to edit an existing entry.
+     */
     private fun openEditDialog(position : Int){
         val dialog = EditEntryDialog(this,viewModel.getEntry(position),position)
         dialog.setCanceledOnTouchOutside(false)
         dialog.create()
         dialog.show()
     }
+
+    /**
+     * Touch helper is providing the recycler view with swipe detection functionality.
+     */
     private fun registerTouchHelper(){
         ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
             override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
@@ -116,23 +129,35 @@ class LedgerActivity : AppCompatActivity() {
         }).attachToRecyclerView(recyclerView)
     }
 
+    /**
+     * Shifts the date one day backward.
+     */
     fun onClickPreviousButton(view: View){
         viewModel.goToPreviousDay()
         adapter.notifyDataSetChanged()
         dateBox.text = DateFormatConverter.parseToDisplayableDate(viewModel.currentDate,this)
     }
 
+    /**
+     * Shifts the date one day forward.
+     */
     fun onClickNextButton(view: View){
         viewModel.goToNextDay()
         adapter.notifyDataSetChanged()
         dateBox.text = DateFormatConverter.parseToDisplayableDate(viewModel.currentDate,this)
     }
 
+    /**
+     * XML button listener. Opens new entry dialog.
+     */
     fun onClickFloatingActionButton(view: View){
         openNewDialog()
         Log.d(tag,"ActionButton")
     }
 
+    /**
+     * XML button listener. Opens the report dialog.
+     */
     private fun onClickLedgerMenuIcon() : Boolean{
         val report = ReportDialog(this)
         report.create()
@@ -141,6 +166,9 @@ class LedgerActivity : AppCompatActivity() {
         return true
     }
 
+    /**
+     * XML button listener. Opens the calendar dialog.
+     */
     private fun onClickCalendarMenuIcon() : Boolean{
         val calendar = CalendarDialog(this)
         calendar.setCanceledOnTouchOutside(false)
@@ -150,6 +178,9 @@ class LedgerActivity : AppCompatActivity() {
         return true
     }
 
+    /**
+     * XML button listener. Opens the search dialog.
+     */
     private fun onClickSearchMenuIcon() : Boolean{
         val search = SearchDialog(this)
         search.setCanceledOnTouchOutside(false)
@@ -159,11 +190,17 @@ class LedgerActivity : AppCompatActivity() {
         return true
     }
 
+    /**
+     * XML button listener. Opens the settings dialog.
+     */
     private fun onClickSettingsMenuIcon() : Boolean{
         Log.d(tag,"Settings")
         return true
     }
 
+    /**
+     * XML button listener. Logs the user out.
+     */
     private fun onClickLogoutMenuIcon() : Boolean{
         val preferences = getSharedPreferences("autoLogin", Context.MODE_PRIVATE)
         val editor = preferences.edit()
@@ -173,18 +210,24 @@ class LedgerActivity : AppCompatActivity() {
         return true
     }
 
+    /**
+     * Displayed the results of a search summary.
+     */
     fun loadSearchResults(summary: Summary){
         viewModel.loadEntriesOfSearch(summary)
         adapter.notifyDataSetChanged()
-        dateBox.text = "${DateFormatConverter.parseToDisplayableFormat(summary.fromDate.toString())} to ${DateFormatConverter.parseToDisplayableFormat(
-            summary.untilDate.toString()
-        )}"
+        val date = DateFormatConverter.parseToDisplayableFormat(summary.fromDate.toString()) + " to " +
+                   DateFormatConverter.parseToDisplayableFormat(summary.untilDate.toString())
+        dateBox.text = date
         nextDayButton.visibility = View.INVISIBLE
         previousDayButton.visibility = View.INVISIBLE
         newEntryButton.visibility = View.INVISIBLE
         searchResultsAreDisplayed = true
     }
 
+    /**
+     * If a search summary is displayed it is cancelled.
+     */
     override fun onBackPressed() {
         if (searchResultsAreDisplayed){
             searchResultsAreDisplayed = false
@@ -200,6 +243,9 @@ class LedgerActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Unregisters the broadcast receiver to avoid leaks and issues a data push request.
+     */
     override fun onPause() {
         super.onPause()
         unregisterReceiver(receiver)
@@ -208,6 +254,9 @@ class LedgerActivity : AppCompatActivity() {
         viewModel.pushModifiedDataToRemoteDatabase()
     }
 
+    /**
+     * Registers the broadcast receiver.
+     */
     override fun onResume() {
         super.onResume()
         val filter = IntentFilter()
@@ -217,20 +266,32 @@ class LedgerActivity : AppCompatActivity() {
         registerReceiver(receiver,filter)
     }
 
+    /**
+     * Hides the synchronization progress bar.
+     */
     fun hideSynchronizeProgressBar(){
         synchronizeLabel.visibility = View.INVISIBLE
         synchronizeProgressBar.visibility = View.INVISIBLE
     }
 
-    fun showSynchronizeProgressBar(){
+    /**
+     * Displays the synchronization progress bar.
+     */
+    private fun showSynchronizeProgressBar(){
         synchronizeLabel.visibility = View.VISIBLE
         synchronizeProgressBar.visibility = View.VISIBLE
     }
 
+    /**
+     * Saves the datetime of the last pull request to the Shared Preferences.
+     */
     fun updateLastPullRequestDatetime(){
         getSharedPreferences("Preferences${viewModel.currentUser}",Context.MODE_PRIVATE).edit().putString("lastPullRequestDatetime",Datetime.currentDatetime()).apply()
     }
 
+    /**
+     * Saves the datetime of the last push request to the Shared Preferences.
+     */
     fun updateLastPushRequestDatetime(){
         getSharedPreferences("Preferences${viewModel.currentUser}",Context.MODE_PRIVATE).edit().putString("lastPushRequestDatetime",Datetime.currentDatetime()).apply()
     }
