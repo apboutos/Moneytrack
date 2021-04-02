6515005a6e252e71d14b602b8b7accf89b582d58
exophrenik <exophrenik@gmail.com>
2021-04-02 12:19:59 +0300

    Added migration strategy 2 -> 3
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/model/database/database/MoneytrackDatabase.kt
+++ app/src/main/java/com/apboutos/moneytrack/model/database/database/MoneytrackDatabase.kt
@@ -7,6 +7,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.apboutos.moneytrack.model.database.converter.Date
 import com.apboutos.moneytrack.model.database.converter.Datetime
 import com.apboutos.moneytrack.model.database.dao.*
@@ -27,6 +29,7 @@ abstract class MoneytrackDatabase : RoomDatabase() {
         @Volatile
         private var instance : MoneytrackDatabase? = null
         private var LOCK = Any()
+        private const val TAG = "MoneytrackDatabase"
 
         /**
          * Returns the current instance of the MoneytrackDatabase and if it is null creates a new instance in a thread safe manner.
@@ -41,7 +44,21 @@ abstract class MoneytrackDatabase : RoomDatabase() {
          * the database.
          */
         private fun buildDatabase(context: Context) : MoneytrackDatabase {
-            return Room.databaseBuilder(context, MoneytrackDatabase::class.java, "Moneytrack_Database.db").fallbackToDestructiveMigration().build()
+            return Room.databaseBuilder(context, MoneytrackDatabase::class.java, "Moneytrack_Database.db")
+                .addMigrations(MIGRATION_2_3).build()
         }
+
+        /**
+         * Migration Strategy from version 2 to version 3.
+         * A migration strategy is required so the data are not lost when moving to a new database schema.
+         */
+        private val MIGRATION_2_3 = object : Migration(2, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+
+            }
+        }
+
     }
+
+
 }
