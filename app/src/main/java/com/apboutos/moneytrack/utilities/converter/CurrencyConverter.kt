622faeb72ca6c197ffc152c6e7129245034d4b52
exophrenik <exophrenik@gmail.com>
2021-04-02 08:09:44 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/utilities/converter/CurrencyConverter.kt
+++ app/src/main/java/com/apboutos/moneytrack/utilities/converter/CurrencyConverter.kt
@@ -14,7 +14,7 @@ object CurrencyConverter {
      */
     fun toPresentableAmount(amount: Double, currency: String) : String{
         if(amount == 0.0) return "0.00 $"
-        val formatter = DecimalFormat("##,###.00");
+        val formatter = DecimalFormat("##,###.00")
 
         return "${formatter.format(amount)} $currency"
     }
