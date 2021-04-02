9834e7c414dc07cf5c663ca4da27f28e8b90a209
exophrenik <exophrenik@gmail.com>
2021-04-02 09:17:05 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/viewmodel/RetrieveActivityViewModel.kt
+++ app/src/main/java/com/apboutos/moneytrack/viewmodel/RetrieveActivityViewModel.kt
@@ -6,6 +6,9 @@ import com.apboutos.moneytrack.model.repository.remote.OnlineRepository
 
 class RetrieveActivityViewModel(application: Application) : AndroidViewModel(application) {
 
+    /**
+     * Sends a request to the server to email the user his credentials.
+     */
     fun retrieveLostCredentials(email: String){
         OnlineRepository(getApplication()).retrieveLostCredentials(email)
     }
