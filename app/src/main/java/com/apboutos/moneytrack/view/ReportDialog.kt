1c50c2703d2bb91d5599106159cfdf9763cb107f
exophrenik <exophrenik@gmail.com>
2021-04-02 09:11:58 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/view/ReportDialog.kt
+++ app/src/main/java/com/apboutos/moneytrack/view/ReportDialog.kt
@@ -100,6 +100,9 @@ class ReportDialog(private val parentActivity: LedgerActivity) : Dialog(parentAc
         }
     }
 
+    /**
+     * Displays this day's sum amount of entries.
+     */
     private fun calculateDailySum(){
         currentDay = DateFormatConverter.normalizeDay(daySpinner.selectedItem.toString())
         val sum = parentActivity.viewModel.getSumOfDateRange(Date("$currentYear-$currentMonth-$currentDay"),Date("$currentYear-$currentMonth-$currentDay"))
@@ -112,6 +115,9 @@ class ReportDialog(private val parentActivity: LedgerActivity) : Dialog(parentAc
         Log.d(tag,"TodaySum= $sum")
     }
 
+    /**
+     * Displays this month's sum amount of entries.
+     */
     private fun calculateMonthlySum(){
         currentMonth = DateFormatConverter.getMonthNumber(monthSpinner.selectedItem.toString(),parentActivity)
         val sum = parentActivity.viewModel.getSumOfDateRange(Date("$currentYear-$currentMonth-01"),Date("$currentYear-$currentMonth-${DateFormatConverter.getLastDayOfMonth(currentMonth)}"))
@@ -124,6 +130,9 @@ class ReportDialog(private val parentActivity: LedgerActivity) : Dialog(parentAc
         Log.d(tag,"MonthlySum= $sum")
     }
 
+    /**
+     * Displays this year's sum amount of entries.
+     */
     private fun calculateYearlySum(){
         currentYear = yearSpinner.selectedItem.toString()
         val sum = parentActivity.viewModel.getSumOfDateRange(Date("$currentYear-01-01"),Date("$currentYear-12-31"))
@@ -136,6 +145,9 @@ class ReportDialog(private val parentActivity: LedgerActivity) : Dialog(parentAc
         Log.d(tag,"YearlySum= $sum")
     }
 
+    /**
+     * Displays the lifetime sum amount of entries.
+     */
     private fun getLifetimeSum(){
         val sum = parentActivity.viewModel.getSumOfLifetime()
         lifetimeSum.text = CurrencyConverter.toPresentableAmount(sum,currency)
@@ -147,6 +159,9 @@ class ReportDialog(private val parentActivity: LedgerActivity) : Dialog(parentAc
         Log.d(tag,"LifetimeSum= $sum")
     }
 
+    /**
+     * Initializes the current date.
+     */
     private fun setUpStartingDate(){
         val parts = parentActivity.viewModel.currentDate.split("-".toRegex()).toTypedArray()
         currentYear = parts[0]
@@ -155,18 +170,27 @@ class ReportDialog(private val parentActivity: LedgerActivity) : Dialog(parentAc
         Log.d(tag,"Day = $currentDay  Month = $currentMonth Year = $currentYear")
     }
 
+    /**
+     * Initializes the day spinner with a list of values corresponding to the current month and year.
+     */
     private fun setUpDaySpinner(){
         val typeAdapter = ArrayAdapter(context, R.layout.activity_ledger_report_spinner, DateFormatConverter.getDaysOfMonth(currentMonth,currentYear))
         daySpinner.adapter = typeAdapter
         daySpinner.setSelection(typeAdapter.getPosition(DateFormatConverter.cropStartingZeroFrom(currentDay)))
     }
 
+    /**
+     * Initializes the month spinner with the proper values.
+     */
     private fun setUpMonthSpinner(){
         val typeAdapter = ArrayAdapter(context,R.layout.activity_ledger_report_spinner, DateFormatConverter.getListOfMonths(parentActivity))
         monthSpinner.adapter = typeAdapter
         monthSpinner.setSelection(typeAdapter.getPosition(DateFormatConverter.getMonthName(currentMonth,parentActivity)))
     }
 
+    /**
+     * Initializes the year spinner with the proper years.
+     */
     private fun setUpYearSpinner(){
         val typeAdapter = ArrayAdapter(context,R.layout.activity_ledger_report_spinner, getYearsThatContainEntries())
         yearSpinner.adapter = typeAdapter
@@ -174,7 +198,10 @@ class ReportDialog(private val parentActivity: LedgerActivity) : Dialog(parentAc
     }
 
 
-
+    /**
+     * Queries the private repository for a list of years that contain entries.
+     * This way only relevant years a displayed as a search option.
+     */
     private fun getYearsThatContainEntries() : Array<String>{
         return parentActivity.viewModel.getYearsThatContainEntries()
     }
