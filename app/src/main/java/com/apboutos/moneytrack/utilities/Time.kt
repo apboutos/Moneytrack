10ee1e45d1687289f0f243431cb4c9c2fc27eb0f
exophrenik <exophrenik@gmail.com>
2021-04-02 08:08:03 +0300

    Documentation.
    Signed-off-by: exophrenik <exophrenik@gmail.com>

--- app/src/main/java/com/apboutos/moneytrack/utilities/Time.kt
+++ app/src/main/java/com/apboutos/moneytrack/utilities/Time.kt
@@ -9,14 +9,23 @@ import java.util.*
 
 object Time {
 
+    /**
+     * Returns a Datetime containing the current timestamp.
+     */
     fun getTimestamp() : Datetime{
-        return Datetime(SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(java.util.Date()))
+        return Datetime(SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(java.util.Date()))
     }
 
+    /**
+     * Returns a Date containing the current date.
+     */
     fun getDate() : Date{
-        return Date(SimpleDateFormat("yyyy-MM-dd", Locale.US).format(java.util.Date()))
+        return Date(SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(java.util.Date()))
     }
 
+    /**
+     * Returns a String containing the current timestamp in yyyyMMddHHmmss format to be used as part of an Entry id.
+     */
     fun getIdTimestamp() : String {
         return SimpleDateFormat("yyyyMMddHHmmss", Locale.US).format(java.util.Date())
     }
